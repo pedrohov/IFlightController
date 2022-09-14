48727e522cfa281b6ba20bc6bc821f0057fac1d3
Pedro <pedrohoveloso@gmail.com>
2022-09-14 17:23:34 -0300

    Add airport and heliport wms layers

--- controller/src/components/MapView/MapView.tsx
+++ controller/src/components/MapView/MapView.tsx
@@ -2,6 +2,7 @@ import TileLayer from "ol/layer/Tile";
 import Map from "ol/Map";
 import OSM from "ol/source/OSM";
 import View from "ol/View";
+import TileWMS from "ol/source/TileWMS";
 import React, { useEffect, useRef, useState } from "react";
 import { MapViewWrapper } from "./Styles";
 
@@ -10,12 +11,26 @@ const MapView = (props) => {
   const [map, setMap] = useState<null | Map>(null);
 
   useEffect(() => {
+    const osm = new TileLayer({
+      source: new OSM(),
+    });
+
+    const airports = new TileLayer({
+      source: new TileWMS({
+        url: "https://geoaisweb.decea.mil.br/geoserver/ICA/wms",
+        params: { LAYERS: "ICA:airport" },
+      }),
+    });
+
+    const heliports = new TileLayer({
+      source: new TileWMS({
+        url: "https://geoaisweb.decea.mil.br/geoserver/ICA/wms",
+        params: { LAYERS: "ICA:heliport" },
+      }),
+    });
+
     const map = new Map({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
+      layers: [osm, airports, heliports],
       controls: [],
       view: new View({
         center: [-6500000, -1700000],
